b9f3a0514846fec09d6303617ff1e83edcff53aa
Wijayaac <wijayaac25@gmail.com>
2024-08-15 21:33:44 +0700

    feat: starter theme nova admin

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAppStore } from "./store";
+import { naiveI18nOptions } from "@/utils";
+
+const appStore = useAppStore();
+
+const naiveLocale = computed(() => {
+  return naiveI18nOptions[appStore.lang] ? naiveI18nOptions[appStore.lang] : naiveI18nOptions.enUS;
+});
+</script>
+
+<template>
+  <n-config-provider class="wh-full" inline-theme-disabled :locale="naiveLocale.locale" :date-locale="naiveLocale.dateLocale" :theme-overrides="appStore.theme">
+    <naive-provider>
+      <router-view />
+      <Watermark :show-watermark="appStore.showWatermark" />
+    </naive-provider>
+  </n-config-provider>
+</template>
---
+++ src/views/account/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Account pages</h1>
+</template>
